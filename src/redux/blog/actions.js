85aa94ad42cf99f058aabe29850201a929851c01
Sharjeel Baig <02-132212-044@student.bahria.edu.pk>
2023-06-04 19:07:09 -0700

    changed the url to geberate from

--- src/redux/blog/actions.js
+++ src/redux/blog/actions.js
@@ -11,7 +11,7 @@ export const generateResponse = (prompt) => {
     const config = {
       method: "post",
       maxBodyLength: Infinity,
-      url: "https://blog-generator-backend.vercel.app/generate",
+      url: "https://blogify.azurewebsites.net/generate",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded"
       },
